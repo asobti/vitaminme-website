42a7f78c296d5eb0f666edd3044be9686079997f
Ayush Sobti <ayushsobti@gmail.com>
2013-04-11 19:10:36 -0400

    changed db pw

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'vitme';
-$db['default']['password'] = 'vitme';
+$db['default']['password'] = 'Drexel2012';
 $db['default']['database'] = 'vitme';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
